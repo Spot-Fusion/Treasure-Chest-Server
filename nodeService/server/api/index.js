adcfda9a0e3437bc6303b4535241882b6ecf92d2
chris <christopherleboeuf1@gmail.com>
2020-03-19 22:41:27 -0500

    Setup routers and queries for following users

--- nodeService/server/api/index.js
+++ nodeService/server/api/index.js
@@ -2,10 +2,12 @@ const combineRouters = require('koa-combine-routers');
 const { userRouter } = require('./routes/user');
 const { listingRouter } = require('./routes/listing');
 const { messageRouter } = require('./routes/message');
+const { followRouter } = require('./routes/follow');
 
 const router = combineRouters(
   userRouter,
   listingRouter,
+  followRouter,
   messageRouter,
 )
 
