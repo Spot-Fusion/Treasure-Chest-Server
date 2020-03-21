f07014b02c89622e2d5da66ee0a674a3b6700474
chris <christopherleboeuf1@gmail.com>
2020-03-20 22:55:31 -0500

    Added route for testing

--- nodeService/server/api/index.js
+++ nodeService/server/api/index.js
@@ -1,10 +1,12 @@
 const combineRouters = require('koa-combine-routers');
+const { testRouter } = require('./routes/test');
 const { userRouter } = require('./routes/user');
 const { listingRouter } = require('./routes/listing');
 const { messageRouter } = require('./routes/message');
 const { followRouter } = require('./routes/follow');
 
 const router = combineRouters(
+  testRouter,
   userRouter,
   listingRouter,
   followRouter,
